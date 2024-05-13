1628841ea9da3ba3cb75e24d6a165d8b8ff7dc96
bbaygx <bayuputraefendi993@gmail.com>
2024-05-13 18:53:55 +0700

    add adapter and jwt strategy

---
+++ src/auth.ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth";
+import prisma from "./lib/db";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import authConfig from "./auth.config";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
